5c87b318f0ab7b6fe995313aac835ed4f59c55e1
triptimishra09 <triptimishra908@gmail.com>
2026-04-06 22:42:31 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -14,3 +14,23 @@ JOIN Books b ON br.book_id = b.book_id;
 SELECT title, available_copies
 FROM Books
 WHERE available_copies > 0;
+
+-- Most Borrowed Books
+SELECT b.title, COUNT(*) AS times_borrowed
+FROM Borrow_Records br
+JOIN Books b ON br.book_id = b.book_id
+GROUP BY b.title
+ORDER BY times_borrowed DESC;
+
+-- Active Members (who borrowed books)
+SELECT m.name, COUNT(*) AS books_taken
+FROM Borrow_Records br
+JOIN Members m ON br.member_id = m.member_id
+GROUP BY m.name;
+
+-- Currently Borrowed Books (not returned)
+SELECT m.name, b.title, br.issue_date
+FROM Borrow_Records br
+JOIN Members m ON br.member_id = m.member_id
+JOIN Books b ON br.book_id = b.book_id
+WHERE br.return_date IS NULL;
